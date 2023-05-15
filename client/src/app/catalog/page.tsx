9b563854ce90890935502cfe9d7161cb2ad75bbc
fenroe <scrambles9494@gmail.com>
2023-05-15 14:32:15 +0300

    adjusted mobile layout of header and restarted work on nav sideboard

--- client/src/app/catalog/page.tsx
+++ client/src/app/catalog/page.tsx
@@ -6,6 +6,7 @@ import { MdDarkMode, MdLightMode } from 'react-icons/md'
 import { RxHamburgerMenu } from 'react-icons/rx'
 import { RiShoppingCartLine } from 'react-icons/ri'
 import { useSnapshot } from "valtio"
+import { NavSidebar } from "@/components"
 
 const Catalog = () => {
   const snap = useSnapshot(state)
@@ -20,48 +21,52 @@ const Catalog = () => {
 
   return (
     <>
+      <NavSidebar />
       <header className={`sticky top-0 px-12 border-b-2 ${snap.darkTheme ? 'header-dark' : 'header-light'}`}>
-        <div className="w-full py-3 flex justify-center items-center md:hidden">
-            <Link className="text-xl"  href="/catalog">
-              <span>Total</span>
-              <span className="text-blue-400">Wash</span>
-            </Link>
-        </div>
         <div className="flex h-20 items-center">
-          <div className="flex-[2] flex sm:flex-1 sm:gap-6">
-            <button className="md:hidden w-12 flex justify-center items-center aspect-square rounded-full" onClick={() => state.showNavSidebar = true}>
+          <div className="flex-[2] flex md:flex-1 sm:gap-6 justify-between">
+            <button className="md:hidden flex justify-center items-center aspect-square rounded-full" onClick={() => state.showNavSidebar = true}>
               <RxHamburgerMenu />
             </button>
-            <Link className="w-min hidden md:inline text-xl"  href="/catalog">
+            <button className="md:hidden flex justify-center items-center aspect-square rounded-full">
+              <AiOutlineSearch />
+            </button>
+            <Link className="w-min text-xl"  href="/catalog">
               <span>Total</span>
               <span className="text-blue-400">Wash</span>
             </Link>
           </div>
-          <div className="flex xxs:flex-[3] sm:flex-[2] xxs:border-2 justify-end  border-black rounded-sm xxs:h-9 xxs:focus-within:border-blue-500">
+          <div className="hidden md:flex flex-[2] border-2 justify-end  border-black rounded-sm h-9 focus-within:border-blue-500">
             <input type="text" className={snap.darkTheme ? 'header-search-input-dark' : 'header-search-input'} placeholder="Search" />
             <button className={snap.darkTheme ? 'header-search-btn-dark' : 'header-search-btn'}>
               <AiOutlineSearch />
             </button>
           </div>
-          <div className="flex-1 flex justify-end">
-            {snap.darkTheme ?            
-            <button
-            onClick={setLightTheme}
-            className="flex items-center text-base gap-3 md:rounded-xl md:w-auto w-12 aspect-square md:aspect-auto rounded-full justify-center hover:underline underline-offset-4">
-              <MdLightMode />
-              <span className="hidden md:inline">Theme</span>
-            </button>
-            :
-            <button
-            onClick={setDarkTheme}
-            className="flex items-center text-base gap-3 md:rounded-xl md:w-auto w-12 aspect-square md:aspect-auto rounded-full justify-center hover:underline underline-offset-4">
-              <MdDarkMode />
-              <span className="hidden md:inline">Theme</span>
-            </button>
-            }
-            <button className="font-bold md:hidden text-2xl w-12 aspect-square rounded-full flex justify-center items-center">
-              <RiShoppingCartLine />
-            </button>
+          <div className="flex-1 flex justify-evenly gap-3 pl-3">
+            <div className="flex flex-1 justify-end">
+              {snap.darkTheme ?            
+              <button
+              onClick={setLightTheme}
+              className="flex items-center text-base gap-3 md:rounded-xl md:w-auto aspect-square md:aspect-auto rounded-full justify-center hover:underline underline-offset-4">
+                <MdLightMode />
+                <span className="hidden md:inline">Theme</span>
+              </button>
+              :
+              <button
+              onClick={setDarkTheme}
+              className="flex items-center text-base gap-3 md:rounded-xl md:w-auto aspect-square md:aspect-auto rounded-full justify-center hover:underline underline-offset-4">
+                <MdDarkMode />
+                <span className="hidden md:inline">Theme</span>
+              </button>
+              }
+            </div>
+            <div className="flex flex-1 justify-end">
+              <button
+              className="flex items-center text-base gap-3 md:rounded-xl md:w-auto aspect-square md:aspect-auto rounded-full justify-center hover:underline underline-offset-4">
+                <RiShoppingCartLine />
+                <span className="hidden md:inline">Cart</span>
+              </button>
+            </div>
           </div>
         </div>
         <nav className="hidden w-full justify-between md:flex text-base flex-nowrap py-3">
@@ -99,12 +104,6 @@ const Catalog = () => {
             <Link href="/catalog/other">
               <span>Other</span>
             </Link>
-          </div>
-          <div className="hover:underline underline-offset-4">
-            <button className="flex items-center gap-3 justify-center">
-              <RiShoppingCartLine />
-              <span className="">Cart</span>
-            </button>
           </div>
         </nav>
       </header>
--- client/src/components/NavSidebar.tsx
+++ client/src/components/NavSidebar.tsx
@@ -5,6 +5,13 @@ export const NavSidebar = () => {
     const snap = useSnapshot(state)
 
     return (
-        <div>Hi</div>
+        <>
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-10 z-10 w-0"/>
+            <div>
+                <nav>
+
+                </nav>
+            </div>
+        </>
     )
 }
