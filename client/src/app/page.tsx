1e5b3ca0cb099c37e8af36df18156bd76f5bb3fd
fenroe <scrambles9494@gmail.com>
2023-06-11 15:49:09 +0300

    feat:updated landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,22 @@
 "use client"
 import Link from 'next/link'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex flex-col justify-center items-start h-screen p-6 bg-landing text-white gap-12">
-      <Link className="absolute top-6 left-6 text-2xl"  href="/catalog">
-        <span>Total</span>
-        <span className="text-blue-500">Wash</span>
-      </Link>
-      <section className="max-w-2xl flex flex-col justify-center items-start gap-12 brightness-100">
-        <h1 className="text-6xl font-bold font-sans">Time to get clean</h1>
-        <h2 className="text-xl font-semibold font-sans leading-10">Your washroom is sacred. At Totalwash we've consolidated everything you'll ever need for your bathroom in one simple store. Whatever you need, we've got you covered.</h2>
-        <Link href="/catalog" className="uppercase bg-white text-black px-9 py-2 rounded-md border-black font-sans font-bold brightness-100 hover:brightness-90 focus:brightness-90">Continue to Store</Link>
+    <main className="bg-landing text-white gap-12 min-h-screen">
+      <section className="p-10 max-w-2xl flex flex-col justify-between gap-12 brightness-100 h-fit my-auto absolute top-1/2 -translate-y-1/2">
+        <div className="max-w-xl">
+          <h1 className="text-5xl font-bold font-sans">The Total Package</h1>
+        </div>
+        <div className="max-w-xl mx-auto">
+          <h2 className="text-xl font-medium font-sans leading-10">Everyone deserves a beautiful bathroom. With an incredible range of washroom furniture across our store, you'll find everything you need to make your dreams a reality.</h2>
+        </div>
+        <div className="max-w-xl">
+          <Link href="/catalog" className="text-center uppercase bg-white text-black px-9 whitespace-nowrap py-2 rounded-md border-black font-sans font-semibold brightness-100 hover:brightness-90 focus:brightness-90">Continue to Store</Link>
+        </div>
       </section>
     </main>
   )
 }
+
+export default Home
